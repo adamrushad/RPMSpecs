674aa575e79ce875c2fc507f638c387170f6ea55
AdamRushad <2429990+adamrushad@users.noreply.github.com>
2018-12-16 15:59:44 -0500

    Create build.sh

---
+++ miniupnpd/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+yum -y install iptables-devel openssl-devel which iproute
+
+cd miniupnp
+MY_VERSION=`cat miniupnpd/VERSION`
+MY_DATE=`git log -1 --date=raw miniupnpd | grep Date | awk '{print $2}'`
+MY_DATE=`TZ="UTC" date --date="@${MY_DATE}" +%Y%m%d%H%M%S`
+cd ${WORKSPACE}
+
+mkdir -p ${WORKSPACE}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+echo -e "%_topdir\t$WORKSPACE/rpmbuild" > ${HOME}/.rpmmacros
+cat ${HOME}/.rpmmacros
+
+curl -O https://raw.githubusercontent.com/adamrushad/RPMSpecs/master/miniupnpd/miniupnpd.spec
+sed -i -e "s/__VERSION__/${MY_VERSION}_${MY_DATE}/" miniupnpd.spec
+mv miniupnpd.spec ${WORKSPACE}/rpmbuild/SPECS/
+
+cd ${WORKSPACE}/rpmbuild/SOURCES/
+curl -O https://raw.githubusercontent.com/adamrushad/RPMSpecs/master/miniupnpd/miniupnpd
+curl -O https://raw.githubusercontent.com/adamrushad/RPMSpecs/master/miniupnpd/miniupnpd.service
+cd ${WORDSPACE}
+
+cp -r miniupnp/miniupnpd miniupnpd-${MY_VERSION}_${MY_DATE}
+tar czf miniupnpd-${MY_VERSION}_${MY_DATE}.tar.gz miniupnpd-${MY_VERSION}_${MY_DATE}/
+mv miniupnpd-${MY_VERSION}_${MY_DATE}.tar.gz ${WORKSPACE}/rpmbuild/SOURCES/
+rm -rf miniupnpd-${MY_VERSION}_${MY_DATE}/
+
+rpmbuild -ba ${WORKSPACE}/rpmbuild/SPECS/miniupnpd.spec
+find ${WORKSPACE}/rpmbuild/
